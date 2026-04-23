bc7794c63a8c0b672342eb9287b39ebcf6abcefd
mjcstudio <martin.creegan@gmail.com>
2026-04-23 11:13:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'flow-v1';
+const ASSETS = ['flow.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
